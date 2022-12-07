da44668b1678f3185459085abf5d96148cded7d0
Anthony Bellancourt <anthony.bellancourt@leviia.com>
2022-12-07 23:51:04 +0100

    day 7: part 2

--- routes/7.ts
+++ routes/7.ts
@@ -23,7 +23,10 @@ function getFiles(lines: string[]): File[] {
                     if (folder && folder !== '..') {
                         if (folder !== "/") folder.replace(/^\//, '');
                         path.push(folder);
-                    } else path.pop();
+                    } else {
+                        // if path.length > 1 then remove the last path
+                        if (path.length > 1) path.pop();
+                    }
                 }
             }
         } else {
@@ -47,28 +50,27 @@ function getFiles(lines: string[]): File[] {
 }
 
 function getFolders(files: File[]): Folders {
-    const folders: Folders = {};
+    const folders: Folders = {"/": 0};
 
     files.forEach(file => {
         if (file.path) {
             const paths = file.path.split("/");
             paths.forEach((path, index) => {
-                if (path === "") return;
+                if (path === "" || path === "/") return;
                 const p = paths.slice(0, index + 1).join("/");
                 if (!folders[p]) folders[p] = 0;
                 folders[p] += file.size;
+                console.log(file.path, p, folders[p]);
             })
         }
+
+        folders["/"] += file.size;
     });
 
     return folders;
 }
 
-function getSize(lines: string[]): number {
-    const files: File[] = getFiles(lines);
-
-    const folders: Folders = getFolders(files);
-
+function getSize(folders: Folders): number {
     let totalSize = 0;
 
     Object.keys(folders).forEach(path => {
@@ -79,15 +81,36 @@ function getSize(lines: string[]): number {
     return totalSize;
 }
 
+function getSizeToFree(folders: Folders) {
+    const foldersCanDelete: Folders = {},
+        totalDiskSize = 70000000,
+        requiredDiskSize = 30000000,
+        rootSize = folders["/"],
+        unusedSpace = totalDiskSize - rootSize,
+        sizeToFree = requiredDiskSize - unusedSpace;
+
+    Object.keys(folders).forEach(path => {
+        // if a folder is bigger than sizeToFree then it can be deleted
+        if (folders[path] > sizeToFree) foldersCanDelete[path] = folders[path];
+    });
+
+    // return the smallest folder size
+    return Math.min(...Object.values(foldersCanDelete));
+}
+
 export const handler = async (_req: Request, _ctx: HandlerContext): Promise<Response> => {
     const resp = await fetch(`http://localhost:8000/api/get_input/7`);
     if (resp.status === 404) return new Response("Not found", {status: 404});
     const lines: string[] = await resp.json();
 
-    const parsedLines = getSize(lines);
+    const folders: Folders = getFolders(getFiles(lines));
+
+    const parsedLines = getSize(folders);
+    const sizeToRemove = getSizeToFree(folders);
 
     return new Response(JSON.stringify({
-        parsedLines
+        parsedLines,
+        sizeToRemove
     }), {
         status: 200,
         headers: {
